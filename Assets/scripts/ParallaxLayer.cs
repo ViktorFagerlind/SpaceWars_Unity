cfb656e23ce11ee4a98d7b01c99ad5b93bbce767
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2013-10-29 22:42:33 +0100

    fixed background layer

---
+++ Assets/scripts/ParallaxLayer.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallaxLayer : MonoBehaviour 
+{
+  public  float m_speed          = 1f;
+  public  float m_size           = 100f;
+  public  float m_threasholdPos  = 0f;
+
+  public  bool  m_print = true;
+  
+	// Use this for initialization
+	void Start () 
+  {
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+	  transform.position = new Vector3 (transform.position.x, transform.position.y - m_speed, transform.position.z);
+    
+    if (transform.position.y < m_threasholdPos)
+      transform.position = new Vector3 (transform.position.x, transform.position.y + m_size, transform.position.z);
+    
+    if (m_print)
+      print (transform.position.y);
+	}
+}
