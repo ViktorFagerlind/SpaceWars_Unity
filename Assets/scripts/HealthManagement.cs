f89d457bb00715519d8719b4dd06a666e42f9941
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2013-10-31 20:31:25 +0100

    Added shot explosions and deletes shots

--- Assets/scripts/HealthManagement.cs
+++ Assets/scripts/HealthManagement.cs
@@ -1,17 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HealthManagement : MonoBehaviour 
+public class HealthManagement : MonoBehaviour
 {
 
-	// Use this for initialization
-	void Start () 
+  // Use this for initialization
+  void Start ()
   {
-	}
-	
-	// Update is called once per frame
-	void Update () 
+  }
+ 
+  // Update is called once per frame
+  void Update ()
   {
-	
-	}
+  }
+  
+  void OnCollisionEnter (Collision collision)
+  {
+    foreach (ContactPoint contact in collision.contacts) 
+    {
+      print (contact.point);
+      
+//      Debug.DrawRay (contact.point, contact.normal, Color.white);
+    }
+    //if (collision.relativeVelocity.magnitude > 2)
+    //  audio.Play ();
+  
+  }
 }
---
+++ Assets/scripts/Weapons/Shot.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shot : MonoBehaviour
+{
+  public GameObject m_explosion;
+  
+  void Update ()
+  {
+    if (!renderer.isVisible)
+      Destroy (gameObject);
+  }
+  
+  void OnCollisionEnter(Collision collision) 
+  {
+    ContactPoint  contact = collision.contacts[0];
+    Quaternion    rot = Quaternion.FromToRotation(Vector3.up, contact.normal);
+    Vector3       pos = contact.point;
+    
+    Instantiate (m_explosion, pos, rot);
+    
+    Destroy (gameObject);
+    
+    print ("hit: " + pos);
+  }
+
+}
