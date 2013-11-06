12a6d26b247e3ed892d1967ac270a4e574fc5f6b
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2013-11-06 19:33:07 +0100

    Smaller update

---
+++ Assets/scripts/Moving/LeanShip.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeanShip : MonoBehaviour 
+{
+  public float m_leanConst     = 0.5f;
+
+	// Use this for initialization
+	void Start () 
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    Vector3 rightForVehicle = new Vector3 (1, 0, 0);
+    rightForVehicle = Quaternion.AngleAxis (transform.rotation.z, Vector3.forward) * rightForVehicle;
+    
+    float leanY = Vector3.Dot (rigidbody.velocity, rightForVehicle) * m_leanConst;
+    
+    transform.localEulerAngles = new Vector3(transform.rotation.eulerAngles.x, -leanY, transform.rotation.eulerAngles.z);
+  }
+}
--- Assets/scripts/Moving/PlayerController.cs
+++ Assets/scripts/Moving/PlayerController.cs
@@ -6,9 +6,6 @@ public class PlayerController : MonoBehaviour
   public float m_moveDistConst = 400;
   public float m_moveCapForce  = 5000;
   
-  public float m_leanConst     = 0.5f;
-  
-  
 	// Use this for initialization
 	void Start () 
 	{
@@ -37,11 +34,5 @@ public class PlayerController : MonoBehaviour
   // Update is called once per frame
   void Update () 
   {
-    Vector3 rightForVehicle = new Vector3 (1, 0, 0);
-    rightForVehicle = Quaternion.AngleAxis (transform.rotation.z, Vector3.forward) * rightForVehicle;
-    
-    float leanY = Vector3.Dot (rigidbody.velocity, rightForVehicle) * m_leanConst;
-    
-    transform.localEulerAngles = new Vector3(transform.rotation.eulerAngles.x, leanY, transform.rotation.eulerAngles.z);
   }
 }
