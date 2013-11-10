f80882ca028e7c5daf0e7ebdcc559fc2a57a8311
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2013-11-10 15:17:24 +0100

    Added nice background

---
+++ Assets/scripts/Flocking/Boid.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Boid : MonoBehaviour
+{
+  private FlockingManager m_flockingManager;  
+  
+  public void setFlockingManager (FlockingManager flockingManager)
+  {
+    m_flockingManager = flockingManager;
+  }
+  
+  void Update ()
+  {
+    Vector3 relativePos = steer () * Time.deltaTime;
+    //       rigidbody.velocity += relativePos; // This is what the Flocking scripts do
+    rigidbody.velocity = steer () * Time.deltaTime; // Alternative  
+    //       transform.position += steer() * Time.deltaTime; // This cause bumpy movements     
+    transform.rotation = Quaternion.LookRotation (relativePos);
+    
+    // enforce minimum and maximum speeds for the boids
+    float speed = rigidbody.velocity.magnitude;
+    if (speed > m_flockingManager.m_maxVelocity) 
+    {
+      rigidbody.velocity = rigidbody.velocity.normalized * m_flockingManager.m_maxVelocity;
+    } 
+    else if (speed < m_flockingManager.m_minVelocity) 
+    {
+      rigidbody.velocity = rigidbody.velocity.normalized * m_flockingManager.m_minVelocity;
+    }
+  }
+  
+  Vector3 steer ()
+  {
+    Vector3 center      = m_flockingManager.flockCenter          - transform.localPosition;      // cohesion
+    Vector3 velocity    = m_flockingManager.flockVelocity        - rigidbody.velocity;       // alignment
+    Vector3 follow      = m_flockingManager.m_target.localPosition - transform.localPosition; // follow leader
+    Vector3 separation  = Vector3.zero;                      // separation
+    
+    foreach (Boid boid in m_flockingManager.m_boids) 
+    {
+      if (boid != this) 
+      {
+        Vector3 relativePos = transform.localPosition - boid.transform.localPosition;
+        separation += relativePos / (relativePos.sqrMagnitude);       
+      }
+    }
+ 
+    // 3D space   
+    Vector3 randomize = new Vector3 ((Random.value * 2) - 1, (Random.value * 2) - 1, (Random.value * 2) - 1);// randomize
+ 
+    // 2D space
+    // Vector3 randomize = new Vector3((Random.value * 2) - 1, 0, (Random.value * 2) - 1);   
+ 
+    randomize.Normalize ();
+ 
+    return (m_flockingManager.m_centerWeight       * center      + 
+            m_flockingManager.m_velocityWeight     * velocity    + 
+            m_flockingManager.m_separationWeight   * separation  + 
+            m_flockingManager.m_followWeight       * follow      + 
+            m_flockingManager.m_randomizeWeight    * randomize);
+  }
+  
+}
---
+++ Assets/scripts/Flocking/FlockingManager.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FlockingManager : MonoBehaviour
+{
+  public float      m_minVelocity       = 1;
+  public float      m_maxVelocity       = 8;
+  public int        m_flockSize         = 20;
+  public float      m_centerWeight      = 1;
+  public float      m_velocityWeight    = 1;
+  public float      m_separationWeight  = 1;
+  public float      m_followWeight      = 1;
+  public float      m_randomizeWeight   = 1;
+  public Boid       m_prefab;
+  public Transform  m_target;
+  public List<Boid> m_boids = new List<Boid> ();  
+  
+  private Vector3   m_flockCenter;
+  private Vector3   m_flockVelocity;
+  
+  public Vector3 flockCenter
+  {
+    get { return m_flockCenter;  }
+    set { m_flockCenter = value; }
+  }
+  
+  public Vector3 flockVelocity
+  {
+    get { return m_flockVelocity;  }
+    set { m_flockVelocity = value; }
+  }
+  
+  // Use this for initialization
+  void Start ()
+  {
+    for (int i = 0; i < m_flockSize; i++) 
+    {
+      Boid boid = Instantiate (m_prefab, transform.position, transform.rotation) as Boid;
+      boid.transform.parent = transform;
+      boid.transform.localPosition = new Vector3 (Random.value * collider.bounds.size.x,
+                                                  Random.value * collider.bounds.size.y,
+                                                  Random.value * collider.bounds.size.z) - collider.bounds.extents;
+      
+      boid.setFlockingManager (this);
+      m_boids.Add (boid);
+    }
+  }
+ 
+  // Update is called once per frame
+  void Update ()
+  {
+    Vector3 center = Vector3.zero;
+    Vector3 velocity = Vector3.zero;
+    
+    foreach (Boid boid in m_boids) 
+    {
+      center   += boid.transform.localPosition;
+      velocity += boid.rigidbody.velocity;
+    }
+    
+    m_flockCenter   = center   / m_flockSize;
+    m_flockVelocity = velocity / m_flockSize;
+  }
+}
