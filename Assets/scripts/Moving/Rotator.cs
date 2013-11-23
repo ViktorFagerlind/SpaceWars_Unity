8c8fee979173eb0ba62f295c3bbbbe6e4976ca88
ViktorFagerlind <viktor_fagerlind@hotmail.com>
2013-11-23 20:06:54 +0100

    Added resources and Rock with turret enemy

---
+++ Assets/scripts/Moving/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+  public Vector3 m_rotation_speed;
+    
+  // Use this for initialization
+  void Start ()
+  {
+  }
+  
+  // Update is called once per frame
+  void Update ()
+  {
+    transform.Rotate (m_rotation_speed);
+  }
+}
